396f98f4380a898537641166b0a555f965138081
xmannj2 <xmannj2@hotmail.com>
2013-11-27 18:42:38 -0800

    Commands update

--- Bot.js
+++ Bot.js
@@ -1,6 +1,9 @@
 function HypixelBot(){
 bot = {
-    skip:
+    skip: function(data) { if (data.message == '!skip' && API.hasPermission(data.fromID, API.ROLE.BOUNCER)) API.moderateForceSkip()},
+    lock: function(data) { if (data.message == '!lock' && API.hasPermission(data.fromID, API.ROLE.MANAGER)) API.moderateLockWaitList(true,false)},
 	
  }
 
+	API.on(API.CHAT, bot.skip, this);
+	API.on(API.CHAT, bot.lock, this);

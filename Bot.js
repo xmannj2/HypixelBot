c6ca5fda2ded180e1a484f85b4cd4e63fcef5313
xmannj2 <xmannj2@hotmail.com>
2013-11-27 18:45:16 -0800

    Commands update

--- Bot.js
+++ Bot.js
@@ -3,8 +3,10 @@ bot = {
     skip: function(data) { if (data.message == '!skip' && API.hasPermission(data.fromID, API.ROLE.BOUNCER)) API.moderateForceSkip()},
     lock: function(data) { if (data.message == '!lock' && API.hasPermission(data.fromID, API.ROLE.MANAGER)) API.moderateLockWaitList(true,false)},
     unlock: function(data) { if (data.message == '!unlock' && API.hasPermission(data.fromID, API.ROLE.MANAGER)) API.moderateLockWaitList(false,false)},
+	meh: function(data) {if (data.message == '!meh' && API.hasPermission (data.fromID, API.ROLE.BOUNCER)) API.sendChat("Reserve Mehs for songs that are extremely overplayed, absolutely awful, or troll songs.")},
  }
 
 	API.on(API.CHAT, bot.skip, this);
 	API.on(API.CHAT, bot.lock, this);
 	API.on(API.CHAT, bot.unlock, this);
+	API.on(API.CHAT, bot.meh, this);
